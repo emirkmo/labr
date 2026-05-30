0af674f926250a46ba4c5e63fbb278bc058e69e6
emirkmo <emirk@axis.com>
2026-05-30 16:06:06 +0200

    make server side only

--- src/main/java/com/example/labr/Labr.java
+++ src/main/java/com/example/labr/Labr.java
@@ -1,7 +1,11 @@
 package com.example.labr;
 
 import com.pixelmonmod.pixelmon.Pixelmon;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,6 +17,14 @@ public final class Labr
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     public Labr() {
+        ModLoadingContext.get().registerExtensionPoint(
+            ExtensionPoint.DISPLAYTEST,
+            () -> Pair.of(
+                () -> FMLNetworkConstants.IGNORESERVERONLY,
+                (remoteVersion, isNetworkConnection) -> true
+            )
+        );
+
         Pixelmon.EVENT_BUS.register(new PixelmonSpawnRandomizer());
         LOGGER.info("Registered Pixelmon spawn randomizer");
     }
